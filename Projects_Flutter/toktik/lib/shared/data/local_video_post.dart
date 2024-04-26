fa84ebc1a954ad6785eeeb71fc1450d22e763123
Pepe <pepewee07@gmail.com>
2024-04-26 15:57:03 -0500

    correccion de repositorio sin videos

---
+++ Projects_Flutter/toktik/lib/shared/data/local_video_post.dart
@@ -0,0 +1,52 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Puentes del lago',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 1023,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 14420,
+    'views': 24230,
+  },
+  {
+    'name': 'Lago de agua dulce, ¡Que hermoso',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 102643,
+    'views': 123563,
+  },
+  {
+    'name': 'Pato caminado por el lago',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 1320,
+    'views': 2300,
+  },
+  {
+    'name': 'Una entidad te asecha',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 301230,
+    'views': 371030,
+  },
+  {
+    'name': 'Un gato en el arbol',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 1560,
+    'views': 3630,
+  },
+  {
+    'name': 'Un Gato muy Rico',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Nino curioso',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 2334553,
+    'views': 3000000,
+  },
+
+
+];
