4cf6d58eece068ac1ace3acda2b315b72e48d30a
Pepe <pepewee07@gmail.com>
2024-04-26 16:59:19 -0500

    controler video

--- Projects_Flutter/toktik/lib/presentation/widgets/video/fullSreenPlayer.dart
+++ Projects_Flutter/toktik/lib/presentation/widgets/video/fullSreenPlayer.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 
 
-class FullSreenPlayer extends StatelessWidget {
+class FullSreenPlayer extends StatefulWidget {
 
   final String videoUrl;
   final String caption;
@@ -10,7 +11,43 @@ class FullSreenPlayer extends StatelessWidget {
   const FullSreenPlayer({super.key, required this.videoUrl, required this.caption});
 
   @override
+  State<FullSreenPlayer> createState() => _FullSreenPlayerState();
+}
+
+class _FullSreenPlayerState extends State<FullSreenPlayer> {
+
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: controller.initialize(), 
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: VideoPlayer(controller),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+        }
+      }
+      );
   }
 }
